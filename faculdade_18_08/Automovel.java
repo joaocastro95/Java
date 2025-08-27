86658106f3605e489df42e3213eeddd9b3a7f591
Joao Victor Oliveira de Castro <joaov.castro95@gmail.com>
2025-08-27 00:24:39 -0300

    aula 18/08

---
+++ faculdade_18_08/Automovel.java
@@ -0,0 +1,17 @@
+package faculdade_18_08;
+
+public class Automovel {
+
+    String marca;
+    String modelo;
+    String cor;
+    double preco;
+
+    public void Informa_Automovel() {
+
+        System.out.println("MARCA:  " + marca);
+        System.out.println("MODELO: " + modelo);
+        System.out.println("COR:    " + cor);
+        System.out.println("PREÇO:  " + preco);
+    }
+}
